00812bc35695ee2817ab598d7846a1518c52eabc
haael <haael@interia.pl>
2019-03-10 12:27:05 +0000

    Account creation and retrieval.

--- operations.js
+++ operations.js
@@ -3,32 +3,51 @@
 const verify = require('./verify');
 const create = require('./create');
 
-
 const createIndividualAccount = require('./helpers/accounts/createIndividualAccount');
 const createFile = require('./helpers/files/createFile');
 const getProof = require('./helpers/files/getProofs');
 
+const fs = require('fs');
+
+function saveFileToDisk(filename, dataString) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filename, dataString, function(err) {
+      if(err)
+          reject(err);
+      else
+          resolve(null);
+    })
+  });
+}
+
+function loadFileFromDisk(filename) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filename, function(err, data) {
+      if(err)
+          reject(err);
+      else
+          resolve(data);
+    });
+  });
+}
 
 async function initIdentity(email, firstname, lastname) {
   const keys = verify.createRSAKeys();
   const privKey = keys.prvKeyObj;
   const pubKey = keys.pubKeyObj;
 
+  const accountId = await createIndividualAccount(email, firstname, lastname);
 
-  // accountData = /* ... */;
-
-  const accountId = await createIndividualAccount(accountData);
-
-  const pubKeyFile = await createFile(accountId, JSON.stringify(pubKey));
+  const pubKeyFile = await createFile(accountId, pubKey);
   const pubKeyFileProof = await getProof(pubKeyFile)
 
   const identity = create.createIdentity(accountId, privKey, pubKeyFile, pubKeyFileProof);
 
-  //await Promise.all([
-  //  saveFileToDisk(accountId + '.key', JSON.stringify(privKey));
-  //  saveFileToDisk(accountId + '.pub_file', pubKeyFile);
-  //  saveFileToDisk(accountId + '.pub_proof', pubKeyFileProof);
-  //]);
+  await Promise.all([
+    saveFileToDisk(accountId + '.key', JSON.stringify(privKey)),
+    saveFileToDisk(accountId + '.pub_file', pubKeyFile),
+    saveFileToDisk(accountId + '.pub_proof', pubKeyFileProof)
+  ]);
 
   return identity;
 }
@@ -36,7 +55,7 @@ async function initIdentity(email, firstname, lastname) {
 
 
 async function retrieveIdentity(accountId) {
-  const privKey = JSON.parse(loadFileFromDisk(accountId + '.key');
+  const privKey = JSON.parse(await loadFileFromDisk(accountId + '.key'));
   const pubKeyFile = await loadFileFromDisk(accountId + '.pub_file');
   const pubKeyFileProof = await loadFileFromDisk(accountId + '.pub_proof');
 
@@ -44,15 +63,13 @@ async function retrieveIdentity(accountId) {
   return identity;
 }
 
+ 
 
 
 
+//initIdentity('haael-6@mailinator.com', "Bartek", "T").then(identity => console.log(identity));
 
-
-
-//initIdentity().then(identity => console.log(identity));
-
-retrieveIdentity("dab9c5f2-0a78-4231-8495-eafe1409f53c").then(identity => console.log(identity));
+retrieveIdentity("0e24b09f-4ea2-4853-8815-93f1ce73454d").then(identity => console.log(identity));
 
 
 
