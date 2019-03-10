aca33ccf3e02bffbf65af25c2f463517c9f9944d
Sumite Ramchande <aiden@Sumites-MacBook-Pro.local>
2019-03-10 12:02:35 +0000

    change file

--- helpers/transactions/getFiles.js
+++ helpers/transactions/getFiles.js
@@ -1,13 +1,16 @@
 const axios = require('axios');
 
-const getFiles = id => axios
-  .get(`${process.env.API_URL}/files/${id}/files?page=1&limit=100`, {
-    headers: {
-      'Content-Type': 'application/json',
-      'x-api-key': process.env.API_KEY,
-    },
-  })
-  .then(res => res.data.data)
-  .catch(error => console.log(error));
+function getFiles(id) {
+  return new Promise((resolve, reject) => {
+    axios.get(`${process.env.API_URL}/files/${id}/files?page=1&limit=100`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': process.env.API_KEY,
+      },
+    })
+    .then(res => resolve(res.data.data))
+    .catch(error => reject(error));
+  });
+}
 
 module.exports = getFiles;
--- helpers/transactions/initiateTransaction.js
+++ helpers/transactions/initiateTransaction.js
@@ -1,13 +1,16 @@
 const axios = require('axios');
 
-const initiateTransaction = data => axios
-  .post(`${process.env.API_URL}/transactions`, data, {
-    headers: {
-      'Content-Type': 'application/json',
-      'x-api-key': process.env.API_KEY,
-    },
-  })
-  .then(res => res.data.data)
-  .catch(error => console.log(error));
+function initiateTransaction(data) {
+  return new Promise((resolve, reject) => {
+    axios.get(`${process.env.API_URL}/transactions`, data, {
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': process.env.API_KEY,
+      },
+    })
+    .then(res => resolve(res.data.data))
+    .catch(error => reject(error));
+  });
+}
 
 module.exports = initiateTransaction;
